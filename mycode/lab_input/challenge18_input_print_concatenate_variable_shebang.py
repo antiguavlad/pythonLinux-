638986a705511e691d4896e90f5849db606ead99
antiguavlad <antiguav@amazon.com>
2023-01-04 17:18:06 +0000

    challege 18

---
+++ mycode/lab_input/challenge18_input_print_concatenate_variable_shebang.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+def main():
+
+    #input asking for the user's name: 
+user_name = input("what is your name? ")
+
+# input asking what day of the week it is: 
+day_of_the_week = input("What day of the week it is? ")
+
+# a print statement that reads as follow: 
+# print statement Hello, <name>! Happy <day of the week>!
+
+print("My name is: " +  user_name + " the day of the week is: " + day_of_the_week)
+
+if __name__ == "__main__":
+    main()
